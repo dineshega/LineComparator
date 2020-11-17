759d0dbcecee13fe4d5fb41eadd7f7c0f45842d4
“dineshega” <dinesh15.ega@gmail.com>
2020-11-17 16:43:19 +0530

    Welcome to Line Comparison Computation Program

---
+++ src/main/java/bridgelabz/linecomparator/Linecomparator.java
@@ -0,0 +1,8 @@
+package bridgelabz.linecomparator;
+
+public class Linecomparator {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line Comparison Computation Program");
+	}
+}
